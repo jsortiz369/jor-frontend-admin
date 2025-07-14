23fedaaa98ca71b665f384f3e09b4eaab2721794
jogan ortiz <jogansmitho@gmail.com>
2025-07-14 07:19:47 -0500

    view component role

--- src/app/modules/roles/features/role-list/role-list.component.ts
+++ src/app/modules/roles/features/role-list/role-list.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { ToastModule } from 'primeng/toast';
 import { PanelModule } from 'primeng/panel';
 import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { TableLazyLoadEvent, TableModule } from 'primeng/table';
 import { MenuItem } from 'primeng/api';
 import { CardModule } from 'primeng/card';
 import { ToolbarModule } from 'primeng/toolbar';
@@ -9,20 +10,51 @@ import { ButtonModule } from 'primeng/button';
 
 import { LoaderService } from '../../../../shared/services';
 import { AlertService } from '../../../../shared/services/alert/alert.service';
+import { RoleAllReponse, RoleDataTable, RoleParamDataTable } from '../../interfaces/role.interface';
+import { RoleService } from '../../services';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-role-list',
-  imports: [ToastModule, PanelModule, BreadcrumbModule, CardModule, ToolbarModule, ButtonModule],
+  imports: [ToastModule, PanelModule, BreadcrumbModule, CardModule, ToolbarModule, ButtonModule, TableModule],
   templateUrl: './role-list.component.html',
 })
 export class RoleListComponent implements OnInit {
   readonly _alertService$ = inject(AlertService);
   readonly _loaderService$ = inject(LoaderService);
+  readonly _roleService$ = inject(RoleService);
 
   protected readonly home = signal<MenuItem>({ icon: 'pi pi-home', label: 'Dashboard', routerLink: '/' });
   protected readonly itemsBreadCrumb = signal<MenuItem[]>([{ label: 'Roles' }]);
 
+  readonly roleTable = signal<RoleDataTable>({ total: 0, rows: 10, loading: false, data: [], event: {} });
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onLazyRoles(event: TableLazyLoadEvent) {
+    this.roleTable.update((last) => ({ ...last, loading: true }));
+    const rows = event?.rows ?? this.roleTable().rows;
+    const dataFilter: RoleParamDataTable = {
+      sort: event.sortField as Pick<RoleParamDataTable, 'sort'>['sort'],
+      limit: rows,
+      page: Math.ceil((event?.first ?? 0) / rows) + 1,
+      sortOrder: event?.sortOrder == 1 ? 'ASC' : 'DESC',
+      search: '', //event?.globalFilter == null ? '' : event?.globalFilter,
+    };
+
+    this._roleService$.findAllByLazy(dataFilter).subscribe({
+      next: ({ data, meta }: RoleAllReponse) => {
+        // const total = meta?.filter != undefined ? meta?.filter : meta.total;
+        // const newData: RoleTableList[] = data.map((_) => ({ ..._, actions: this.menusActions(_) }));
+        // this.roleTable.update(() => ({ rows, total, loading: false, data: newData, event }));
+      },
+      error: (error: HttpErrorResponse) => {
+        this.roleTable.update((last) => ({ ...last, loading: false }));
+        //this._alertService$.clearToast('');
+        //this._alertService$.toast({ severity: 'warn', summary: 'Warn', detail: `the list of roles could not be obtained` });
+      },
+    });
+  }
 }
--- src/app/modules/roles/interfaces/role.interface.ts
+++ src/app/modules/roles/interfaces/role.interface.ts
@@ -1,6 +1,9 @@
+import { TableLazyLoadEvent } from 'primeng/table';
 import { z } from 'zod';
 import { roleAllResponseScheme, roleParamDataTableScheme, roleResponseScheme } from '../shemes/role.scheme';
 
 export type Role = z.infer<typeof roleResponseScheme>;
 export type RoleAllReponse = z.infer<typeof roleAllResponseScheme>;
 export type RoleParamDataTable = z.infer<typeof roleParamDataTableScheme>;
+
+export type RoleDataTable = { total: number; rows: number; event: TableLazyLoadEvent; loading: boolean; data: Role[] };
