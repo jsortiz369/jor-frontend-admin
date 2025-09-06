ea534b2d6d73b3d2684129566a68eb355280f501
jogan ortiz <jogansmitho@gmail.com>
2025-09-06 09:04:50 -0500

    ajust

--- src/app/core/interceptors/timezone-fn/timezone-fn.interceptor.ts
+++ src/app/core/interceptors/timezone-fn/timezone-fn.interceptor.ts
@@ -2,17 +2,6 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const timezoneFnInterceptor: HttpInterceptorFn = (req, next) => {
   const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  const formar = new Intl.DateTimeFormat('en-US', {
-    timeZone: timeZone,
-    timeZoneName: 'shortOffset',
-  });
-
-  const timeZoneName = formar.formatToParts(new Date()).find((p) => p.type === 'timeZoneName')?.value;
-  let offset = timeZoneName?.replace('GMT', '');
-  if (!offset?.includes(':')) {
-    offset = offset + ':00';
-  }
-
-  const modifiedReq = req.clone({ setHeaders: { offset: 'prueba' } });
+  const modifiedReq = req.clone({ setHeaders: { Timezone: timeZone } });
   return next(modifiedReq);
 };
